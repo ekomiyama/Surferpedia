d9cff665cad7ed6d5217256b18e22b7a71d8e7f2
evkomiyama <evkomiyama@gmail.com>
2013-10-06 21:47:47 -1000

    first comit

---
+++ app/controllers/Application.java
@@ -0,0 +1,49 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.Index;
+import views.html.kellyslater;
+import views.html.maliamanuel;
+import views.html.jakemarshall;
+
+/**
+ * Implements the controllers for this application.
+ */
+public class Application extends Controller {
+
+  /**
+   * Returns the home page. 
+   * @return The resulting home page. 
+   */
+  public static Result index() {
+    return ok(Index.render("Welcome to the home page."));
+  }
+  
+  /**
+   * Returns page1, a simple example of a second page to illustrate navigation.
+   * @return The Page1.
+   */
+  public static Result kellyslater() {
+    return ok(kellyslater.render("Kelly Slater"));
+    
+  }
+  
+  /**
+   * Returns page1, a simple example of a second page to illustrate navigation.
+   * @return The Page1.
+   */
+  public static Result maliamanuel() {
+    return ok(maliamanuel.render("Malia Manuel"));
+    
+  }
+  
+  /**
+   * Returns page1, a simple example of a second page to illustrate navigation.
+   * @return The Page1.
+   */
+  public static Result jakemarshall() {
+    return ok(jakemarshall.render("Jake Marshall"));
+    
+  }
+}
